fd6f93dbb9623b1ab7bf48c049b9207292af0ff3
Yash Gupta <yash8917gupta@gmail.com>
2022-01-04 16:55:32 +0530

    Initial commit

---
+++ src/Solid_Pattern.java
@@ -0,0 +1,19 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class Solid_Pattern {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no.of Rows:");
+        int n= sc.nextInt();
+        System.out.println("Enter the no. of Column:");
+        int c= sc.nextInt();
+        for (int i = 1; i <=n; i++) {
+            for(int j=1;j<=c;j++){//this  concept is known as nested loop's
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
