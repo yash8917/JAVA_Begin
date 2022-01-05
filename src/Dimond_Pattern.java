5b64d857f4a1ff98c9c276328eb0dccd779cd635
Yash Gupta <yash8917gupta@gmail.com>
2022-01-05 17:14:22 +0530

    Initial commit

---
+++ src/Dimond_Pattern.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Dimond_Pattern {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no:");
+        int n= sc.nextInt();
+        //upper half
+        for (int i = 1; i <=n ; i++) {
+            //spaces
+            for (int j = 1; j <=n-i ; j++) {
+                System.out.print(" ");
+            }
+            //stars
+            for (int j = 1; j <=2*i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //lower half
+        for (int i = n; i >=1 ; i--) {
+            //spaces
+            for (int j = 1; j <=n-i ; j++) {
+                System.out.print(" ");
+            }
+            //stars
+            for (int j = 1; j <=2*i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
+/*
+Enter the no:
+6
+     *
+    ***
+   *****
+  *******
+ *********
+***********
+***********
+ *********
+  *******
+   *****
+    ***
+     *
+
+ */
