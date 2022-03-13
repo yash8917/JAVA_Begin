6ee287119450c1a010d95c702bb12fa21fcba52b
Yash Gupta <yash8917gupta@gmail.com>
2022-03-13 19:37:34 +0530

    Initial commit

---
+++ src/String/CharAt_Method.java
@@ -0,0 +1,9 @@
+package String;
+
+public class CharAt_Method {
+    public static void main(String[] args) {
+        StringBuilder st=new StringBuilder("Hello");
+        System.out.println(st.charAt(3));//Returns the char value in this sequence at the specified index.
+    }
+
+}
