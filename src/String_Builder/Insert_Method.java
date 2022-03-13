c2fac8baa752e99772e2ab382f2d9d8991162233
Yash Gupta <yash8917gupta@gmail.com>
2022-03-13 19:37:15 +0530

    Initial commit

---
+++ src/String_Builder/Insert_Method.java
@@ -0,0 +1,8 @@
+package String_Builder;
+
+public class Insert_Method {
+    public static void main(String[] args) {
+        StringBuilder str=new StringBuilder("HELLO");
+        System.out.println(str.insert(1,"java"));
+    }
+}
