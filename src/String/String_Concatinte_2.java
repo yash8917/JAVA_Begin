9629d3fefb3293d95b967a98f9f3448c5df478bd
Yash Gupta <yash8917gupta@gmail.com>
2022-03-13 19:24:44 +0530

    Initial commit

---
+++ src/String/String_Concatinte_2.java
@@ -0,0 +1,10 @@
+package String;
+
+public class String_Concatinte_2 {
+    public static void main(String[] args) {
+        String s1="Sachin";
+        String s2=" Tendulkar";
+        String s=String.join("",s1,s2);
+        System.out.println(s.toString());
+    }
+}
