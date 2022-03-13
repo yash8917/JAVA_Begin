66f3be7aae677a6456c3e488f773d812fd07ab84
Yash Gupta <yash8917gupta@gmail.com>
2022-03-13 19:37:44 +0530

    Initial commit

---
+++ src/String_Builder/Append_Method.java
@@ -0,0 +1,8 @@
+package String_Builder;
+
+public class Append_Method {
+    public static void main(String[] args) {
+        StringBuilder str=new StringBuilder("Hello");
+        System.out.println(str.append(" java"));
+    }
+}
