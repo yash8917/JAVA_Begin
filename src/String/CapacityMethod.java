953fab58375d51eeca32932fd5e1668760423a4d
Yash Gupta <yash8917gupta@gmail.com>
2022-03-13 20:01:31 +0530

    Initial commit

---
+++ src/String/CapacityMethod.java
@@ -0,0 +1,17 @@
+package String;
+
+public class CapacityMethod {
+    public static void main(String[] args) {
+        StringBuilder st=new StringBuilder();
+        System.out.println(st.capacity());
+        st.append("Hello idn");
+        System.out.println(st.capacity());
+        st.append("java is there");
+        System.out.println(st.capacity());
+    }
+}
+/*
+16
+16
+34
+*/
