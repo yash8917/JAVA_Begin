30de55381f1d20e989237b4f170907d9c653754f
Yash Gupta <yash8917gupta@gmail.com>
2022-03-31 23:33:22 +0530

    Initial commit

--- src/Pattern/Half_Triangle.java
+++ src/Pattern/Half_Triangle.java
@@ -5,7 +5,7 @@ public class Half_Triangle {
         int n=5;
         for (int i = 0; i <n ; i++) {
             for (int j = 0; j < i; j++) {
-                System.out.println("*");
+                System.out.print("*");
             }
         }
     }
